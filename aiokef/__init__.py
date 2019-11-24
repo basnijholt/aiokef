dd9d000a78ab848f3a8556287226fa7de6eca607
Bas Nijholt <basnijholt@gmail.com>
2019-11-24 20:38:21 +0100

    Bump version: 0.1.7 → 0.1.8.dev0

--- aiokef/__init__.py
+++ aiokef/__init__.py
@@ -1,6 +1,6 @@
 """A module for asynchronously interacting with KEF wireless speakers."""
 
-__version__ = "0.1.7"
+__version__ = "0.1.8.dev0"
 
 from aiokef.aiokef import AsyncKefSpeaker, SyncKefSpeaker
 
